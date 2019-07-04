6cbabe49edd61f900b07b05d72bd870b33dff2ce
Pablo Miranda <pablo.miranda@grability.com>
2019-07-04 17:02:26 -0500

    Add Redux Support

---
+++ app/src/main/java/com/pfmiranda/designpatterns/ReduxActivity.kt
@@ -0,0 +1,12 @@
+package com.pfmiranda.designpatterns
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReduxActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_redux)
+    }
+}
