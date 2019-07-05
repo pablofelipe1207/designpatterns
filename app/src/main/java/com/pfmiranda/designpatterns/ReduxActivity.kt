d33541a3468605b56abd9f469432dd57045a9c82
Pablo Miranda <pablo.miranda@grability.com>
2019-07-05 10:57:33 -0500

    Add Redux Support Action and Reducer and Example in Activity

--- app/src/main/java/com/pfmiranda/designpatterns/ReduxActivity.kt
+++ app/src/main/java/com/pfmiranda/designpatterns/ReduxActivity.kt
@@ -2,11 +2,29 @@ package com.pfmiranda.designpatterns
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.pfmiranda.designpatterns.redux.CounterActions
+import com.pfmiranda.designpatterns.redux.CounterReducer
+import com.yheriatovych.reductor.Actions
+import com.yheriatovych.reductor.Store
 
 class ReduxActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_redux)
+
+        val counterStore = Store.create(CounterReducer.create())
+
+        val actions = Actions.from(CounterActions::class.java)
+
+        counterStore.subscribe { state -> Toast.makeText(this@ReduxActivity, "Valor:"+ state, Toast.LENGTH_LONG).show() }
+
+        counterStore.dispatch(actions.increment()); //print 1
+
+        counterStore.dispatch(actions.increment()); //print 2
+
+        counterStore.dispatch(actions.add(5));      //print 7
+
     }
 }
